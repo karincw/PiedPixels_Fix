804a1616598b25ac7b096b34a6fc99a991eb7608
Karin <karin.cw07@gmail.com>
2025-09-24 23:27:42 +0900

    add : serverCode
    http 방식을 통해 서버에서 받은 Method를 실행하고 출력하는 코드를작성하였습니다.
    테스트를 위해 If문으로 처리했으나 변경할 예정입니다.

--- RpcServer/RpcServer/DTO/RpcRequestDTO.cs
+++ RpcServer/RpcServer/DTO/RpcRequestDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RpcServer.DTO
+﻿namespace RpcServer.DTO
 {
     public record RpcRequestDTO
     {
--- RpcServer/RpcServer/Program.cs
+++ RpcServer/RpcServer/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace RpcServer
+﻿using RpcServer.DTO;
+using System.Text.Json;
+
+namespace RpcServer
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
 
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Game Server is running!");
+
+            app.MapPost("/rpc", async (HttpRequest request) =>
+            {
+                using var reader = new StreamReader(request.Body);
+                var body = await reader.ReadToEndAsync();
+
+                var rpcRequest = JsonSerializer.Deserialize<RpcRequestDTO>(body);
+
+                if (rpcRequest == null)
+                {
+                    return Results.Json(new { error = "Invalid request" });
+                }
+
+                if (rpcRequest.Method == null)
+                {
+                    return Results.Json(new { error = "Method Name Required" });
+                }
+                object? result = null;
+
+                if (rpcRequest.Method == "hello")
+                {
+                    result = $"hello {rpcRequest.Params["name"]}";
+                }
+
+                var response = new RpcResponseDTO
+                {
+                    id = rpcRequest.Id,
+                    result = result
+                };
+
+                return Results.Json(response);
+            });
+
+            app.Run();
         }
     }
 }
