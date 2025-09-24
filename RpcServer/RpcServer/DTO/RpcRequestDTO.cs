d42bcc619b831264a04d0b8506fb9b7880e1154a
Karin <karin.cw07@gmail.com>
2025-09-24 23:04:06 +0900

    add : DTO
    DTO(Data Transfer Object)
    데이터 입력,출력 전용 객체를 생성했습니다.

---
+++ RpcServer/RpcServer/DTO/RpcRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpcServer.DTO
+{
+    public record RpcRequestDTO
+    {
+        public string? JsonRpc { get; set; } = "2.0";
+        public string? Method { get; set; } = "";
+        public Dictionary<string, object>? Params { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ RpcServer/RpcServer/DTO/RpcResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RpcServer.DTO
+{
+    public record RpcResponseDTO
+    {
+        public string? JsonRpc { get; set; } = "2.0";
+        public object? result { get; set; }
+        public object? error { get; set; }
+        public int id { get; set; }
+    }
+}
