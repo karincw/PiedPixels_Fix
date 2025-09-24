ca1d1048797b60f74e4e28926d334b97c54a6b01
Karin <karin.cw07@gmail.com>
2025-09-25 00:16:29 +0900

    add : WebSocket
    기존에 제작한 RpcDispatcher, FormatUtility, SerializerUtility등을 이용해서 WebSocket을 이용한 통신이 가능하도록 구현했습니다.

--- RpcServer/RpcServer/Program.cs
+++ RpcServer/RpcServer/Program.cs
@@ -1,8 +1,8 @@
 ﻿using RpcServer.DTO;
 using RpcServer.Serializer;
 using RPCServer;
-using RPCServer.RpcService;
-using System.Text.Json;
+using System.Buffers;
+using System.Net.WebSockets;
 
 namespace RpcServer
 {
@@ -14,6 +14,7 @@ namespace RpcServer
             builder.Services.AddSingleton<RpcDispatcher>();
 
             var app = builder.Build();
+            app.UseWebSockets();
 
             app.MapGet("/", () => "Game Server is running!");
 
@@ -40,6 +41,82 @@ namespace RpcServer
                     contents: response,
                     contentType: FormatUtility.GetContentType(outputFormat)
                 );
+            });
+
+            app.MapGet("/ws", async (HttpContext context, RpcDispatcher rpcDispatcher) =>
+            {
+                if (!context.WebSockets.IsWebSocketRequest)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
+                }
+
+                using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                byte[] buffer = null;
+
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    try
+                    {
+                        buffer = ArrayPool<byte>.Shared.Rent(1024 * 4);
+
+                        using var memoryStream = new MemoryStream();
+
+                        WebSocketReceiveResult receiveResult;
+                        do
+                        {
+                            receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                            if (receiveResult.MessageType == WebSocketMessageType.Close)
+                            {
+                                await webSocket.CloseAsync(
+                                    WebSocketCloseStatus.NormalClosure,
+                                    "Closed by client",
+                                    CancellationToken.None);
+                                return;
+                            }
+
+                            memoryStream.Write(buffer, 0, receiveResult.Count);
+
+                        } while (!receiveResult.EndOfMessage);
+
+                        if (receiveResult.MessageType != WebSocketMessageType.Text)
+                        {
+                            continue;
+                        }
+
+                        DataFormat inputFormat = FormatUtility.ResolveInput(context);
+                        DataFormat outputFormat = FormatUtility.ResolveOutput(context);
+
+                        RpcRequestDTO rpcRequest = SerializerUtility.DeSerializer(memoryStream.ToArray(), inputFormat);
+
+                        RpcResponseDTO rpcResponse = await rpcDispatcher.DispatchAsync(rpcRequest);
+
+
+                        var responseBytes = SerializerUtility.Serializer(rpcResponse, outputFormat);
+
+                        await webSocket.SendAsync(
+                            new ArraySegment<byte>(responseBytes),
+                            WebSocketMessageType.Text,
+                            endOfMessage: true,
+                            cancellationToken: CancellationToken.None);
+                    }
+                    catch (WebSocketException wsex)
+                    {
+                        Console.WriteLine($"[WebSocketException] {wsex.Message}");
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[Unhandled] {ex.Message}");
+                        break;
+                    }
+                    finally
+                    {
+                        if (buffer != null)
+                            ArrayPool<byte>.Shared.Return(buffer, true);
+                    }
+                }
             });
 
             app.Run();
